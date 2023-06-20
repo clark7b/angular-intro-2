eb88337053ea42e372296bf3b8d42371937cf9e7
clark <clark7b@gmail.com>
2023-06-20 09:44:17 -0500

    added an array of tasks and used it in our html

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
 
   title:string = 'The meaning of life = ';
+  tasks: Task[] = [
+    { title: 'Learn TypeScript', completed: false },
+    { title: 'Learn HTML', completed: false },
+    { title: 'Learn basic english', completed: true }
+  ];
+  constructor() { }
 
+  ngOnInit(): void {
+    console.table(this.tasks);
+  }
+
+}
+interface Task{
+  title: string;
+  completed: boolean;
 }
